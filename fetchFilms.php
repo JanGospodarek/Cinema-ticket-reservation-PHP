675f8f7a00609f498c671309f7382e6cb359ccba
JanGospodarek <jan.gospo@onet.pl>
2023-02-03 12:33:13 +0100

    selecting hour

---
+++ fetchFilms.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+if (!isset($_SESSION["login"])){
+  header("Location: index.php");
+ exit();
+}
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ticket";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT IDFilmu,Nazwa,Data,Opis,Godziny,imgUrl FROM films";
+$result = $conn->query($sql);
+$filmy=[];
+ 
+class filmClass {
+  public $ID;
+     public $nazwa;
+     public $opis;
+     public $data;
+     public $godziny;
+     public $link;
+    
+}
+
+while($row = $result->fetch_assoc()) {
+  $film=new filmClass();
+  $film->nazwa=$row['Nazwa'];
+  $film->ID=$row['IDFilmu'];
+  $film->opis=$row['Opis'];
+  $film->data=$row['Data'];
+  $film->godziny=$row['Godziny'];
+  $film->link=$row['imgUrl'];
+  array_push($filmy,$film );
+}
+// var_dump($filmy)
+// foreach($filmy as $i){
+//   echo 'Nazwa'.$i->nazwa;
+// }
+?>
--- main.php
+++ main.php
@@ -1,49 +1,4 @@
-<?php
-session_start();
-if (!isset($_SESSION["login"])){
-  header("Location: index.php");
- exit();
-}
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ticket";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT IDFilmu,Nazwa,Data,Opis,Godziny,imgUrl FROM films";
-$result = $conn->query($sql);
-$filmy=[];
- 
-class filmClass {
-  
-     public $nazwa;
-     public $opis;
-     public $data;
-     public $godziny;
-     public $link;
-    
-}
-
-while($row = $result->fetch_assoc()) {
-  $film=new filmClass();
-  $film->nazwa=$row['Nazwa'];
-  $film->opis=$row['Opis'];
-  $film->data=$row['Data'];
-  $film->godziny=$row['Godziny'];
-  $film->link=$row['imgUrl'];
-  array_push($filmy,$film );
-}
-// var_dump($filmy)
-// foreach($filmy as $i){
-//   echo 'Nazwa'.$i->nazwa;
-// }
-?>
+<?php include "fetchFilms.php"?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -67,10 +22,10 @@ while($row = $result->fetch_assoc()) {
     </div>
 
 
-    <script>
-        const context={}
+    <script type='module'>
 
       const filmy =<?php echo json_encode($filmy)?>;
+      const context={}
 
       filmy.forEach(film=>{
         const arr=film.godziny.split(",")
@@ -84,17 +39,18 @@ while($row = $result->fetch_assoc()) {
 
       const datySet=new Set(datyArr)
       datyArr= Array.from(datySet)
-
-      datyArr.forEach((el)=>{
+const sorted =datyArr.sort()
+console.log(sorted);
+      sorted.forEach((el)=>{
         const filtered=filmy.filter(film=>film.data==el)
         context[el]=filtered
       })
       let filmyStr=``;
       
-      function godzinyMap(arr){
+      function godzinyMap(obj){
         let str=``
-arr.forEach(element => {
-  str+=`<button class="dataCont">${element}</button>`
+obj.godziny.forEach(el => {
+  str+=`<a href='order.php/?ID=${obj.ID}?godzina=${el}' class="dataCont">${el}</a>`
 });
 return str
       }
@@ -115,7 +71,7 @@ arr.forEach(element => {
         <div class="free">
           <h3>Godziny:</h3>
           <div class="daty">
-          ${godzinyMap(element.godziny)}
+          ${godzinyMap(element)}
           </div>
         </div>
       </div>
@@ -131,7 +87,6 @@ return str
        
        `
       }
-      console.log(filmyStr);
       document.getElementById('center').innerHTML=filmyStr;
 </script>
   </body>
