c297f1b05a25221ed66c88d6d4ba1bf9076c5116
JanGospodarek <jan.gospo@onet.pl>
2023-03-04 11:58:32 +0100

    'dump'

---
+++ ticket (2).sql
@@ -0,0 +1,214 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 04 Mar 2023, 11:58
+-- Wersja serwera: 10.4.27-MariaDB
+-- Wersja PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `ticket`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `films`
+--
+
+CREATE TABLE `films` (
+  `IDFilmu` int(11) NOT NULL,
+  `Nazwa` text NOT NULL,
+  `Opis` text NOT NULL,
+  `imgUrl` text NOT NULL,
+  `Data` date NOT NULL,
+  `Godziny` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Zrzut danych tabeli `films`
+--
+
+INSERT INTO `films` (`IDFilmu`, `Nazwa`, `Opis`, `imgUrl`, `Data`, `Godziny`) VALUES
+(2, 'Avatar: Istota wody', 'Pandorę znów napada wroga korporacja w poszukiwaniu cennych minerałów. Jack i Neytiri wraz z rodziną zmuszeni są opuścić wioskę i szukać pomocy u innych plemion zamieszkujących planetę.', 'https://fwcdn.pl/fpo/81/78/558178/8047434.3.jpg', '2023-02-03', '12:00,12:45'),
+(3, 'James Bond: skyfall', 'Lojalność agenta 007 wobec M zostaje wystawiona na próbę po ataku na siedzibę MI6 i kradzieży tajnych danych. Trop wiedzie do osoby z przeszłości szefowej brytyjskiego wywiadu.', 'https://fwcdn.pl/fpo/12/44/451244/7494882.3.jpg', '2023-02-01', '11:00,15:00'),
+(4, 'Minionki', 'Minionki Kevin, Stuart, Bob oraz Otto muszą ratować młodego Gru, który popadł w konflikt z grupą super złoczyńców znaną jako Vicious 6.', 'https://as.repertuary.pl/images/dbimages/film_21702_original_1.jpg', '2023-02-03', '11:00,15:00'),
+(5, 'Pulp Fiction', 'Przemoc i odkupienie w opowieści o dwóch płatnych mordercach pracujących na zlecenie mafii, żonie gangstera, bokserze i parze okradającej ludzi w restauracji.', 'https://static.posters.cz/image/750/plakaty/pulp-fiction-cover-i1288.jpg', '2023-02-01', '09:00,11:00'),
+(6, 'Dunkierka', 'Wojska alianckie zostają przyparte do morza pod Dunkierką. Bitwa staje się sprawdzianem dla młodych żołnierzy, pilota RAF-u oraz załogi cywilnej łodzi płynącej przez Kanał La Manche.', 'https://fwcdn.pl/fpo/11/41/681141/7797814.3.jpg', '2023-02-10', '14:00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `rezerwacje`
+--
+
+CREATE TABLE `rezerwacje` (
+  `IDRezerwacji` int(11) NOT NULL,
+  `IDSeansu` int(11) NOT NULL,
+  `Godzina` text NOT NULL,
+  `Data` text NOT NULL,
+  `Miejsca` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Zrzut danych tabeli `rezerwacje`
+--
+
+INSERT INTO `rezerwacje` (`IDRezerwacji`, `IDSeansu`, `Godzina`, `Data`, `Miejsca`) VALUES
+(6, 3, '15:00', '2023-02-01', '191,171'),
+(7, 3, '15:00', '2023-02-01', '189,209,208'),
+(8, 4, '11:00', '2023-02-01', '192,191'),
+(9, 4, '11:00', '2023-02-01', '249,248,247'),
+(10, 4, '11:00', '2023-02-01', '91,90,89'),
+(11, 4, '11:00', '2023-02-01', '91,90,89'),
+(12, 4, '11:00', '2023-02-01', '132,130'),
+(13, 9, '14:00', '2023-02-10', '112,111,110'),
+(14, 9, '14:00', '2023-02-10', '190,189,188'),
+(15, 7, '09:00', '2023-02-01', '171,170'),
+(16, 9, '14:00', '2023-02-10', '72,71,70'),
+(17, 4, '11:00', '2023-02-01', '226,225'),
+(18, 4, '11:00', '2023-02-01', '51,50,49'),
+(19, 4, '11:00', '2023-02-01', '51,50,49'),
+(20, 4, '11:00', '2023-02-01', '189'),
+(21, 4, '11:00', '2023-02-01', '189');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `seanse`
+--
+
+CREATE TABLE `seanse` (
+  `IDSeansu` int(11) NOT NULL,
+  `IDFilmu` int(11) NOT NULL,
+  `Godzina` text NOT NULL,
+  `Data` date DEFAULT NULL,
+  `miejsca` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Zrzut danych tabeli `seanse`
+--
+
+INSERT INTO `seanse` (`IDSeansu`, `IDFilmu`, `Godzina`, `Data`, `miejsca`) VALUES
+(1, 2, '12:00', '2023-02-03', '1,2,6,10'),
+(2, 2, '12:45', '2023-02-03', ''),
+(3, 3, '15:00', '2023-02-01', '292,291,290'),
+(4, 3, '11:00', '2023-02-01', '300,299,192,191,249,248,247,91,90,89,132,130,226,225,51,50,49,189'),
+(5, 4, '11:00', '2023-02-03', '12,13'),
+(6, 4, '15:00', '2023-02-03', '1,2,3,4,5'),
+(7, 5, '09:00', '2023-02-01', '200,300,121,171,170'),
+(8, 5, '11:00', '2023-02-01', '11,12,13,145'),
+(9, 6, '14:00', '2023-02-10', ',112,111,110,190,189,188,72,71,70');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `LogIn` text NOT NULL,
+  `Password` text NOT NULL,
+  `Number` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`ID`, `LogIn`, `Password`, `Number`) VALUES
+(4, 'adminwwww', '$2y$10$7/HNNUDAdNRxAhxPOPH2oeg9uAgzAkJc8ED7OqkGAIBMvfREUqibS', '123123123'),
+(5, 'admin', '$2y$10$nhYgh3OzeXDiGFgWqevXRerU8RbFucxZWc5L5/4XpnVhtvRIezvUK', '123123123');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `films`
+--
+ALTER TABLE `films`
+  ADD PRIMARY KEY (`IDFilmu`);
+
+--
+-- Indeksy dla tabeli `rezerwacje`
+--
+ALTER TABLE `rezerwacje`
+  ADD PRIMARY KEY (`IDRezerwacji`),
+  ADD KEY `IDFilmu` (`IDSeansu`),
+  ADD KEY `IDSeansu` (`IDSeansu`);
+
+--
+-- Indeksy dla tabeli `seanse`
+--
+ALTER TABLE `seanse`
+  ADD PRIMARY KEY (`IDSeansu`),
+  ADD KEY `IDFilmu` (`IDFilmu`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `films`
+--
+ALTER TABLE `films`
+  MODIFY `IDFilmu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT dla tabeli `rezerwacje`
+--
+ALTER TABLE `rezerwacje`
+  MODIFY `IDRezerwacji` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT dla tabeli `seanse`
+--
+ALTER TABLE `seanse`
+  MODIFY `IDSeansu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `rezerwacje`
+--
+ALTER TABLE `rezerwacje`
+  ADD CONSTRAINT `rezerwacje_ibfk_1` FOREIGN KEY (`IDSeansu`) REFERENCES `seanse` (`IDSeansu`);
+
+--
+-- Ograniczenia dla tabeli `seanse`
+--
+ALTER TABLE `seanse`
+  ADD CONSTRAINT `seanse_ibfk_1` FOREIGN KEY (`IDFilmu`) REFERENCES `films` (`IDFilmu`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
